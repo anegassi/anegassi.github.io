641802d60f8eb93ea39942bf7cafe217cd68a958
anegassi <89844342+anegassi@users.noreply.github.com>
2021-10-05 07:16:11 -0700

    update

---
+++ exercises/w2d1/quiz.js
@@ -0,0 +1,30 @@
+/**
+ *  Select all that that apply
+ */
+// A. Style your content
+// B. Structure your content
+// C. Define semantics of your content
+// D. Make your page interactive
+// E. Layout your content 
+
+/**
+ * 1. CSS is used to 
+ * Answers A & E
+ * 2. HTML is used to
+ * Answers B & C
+ */
+/**
+ *3. What are the 3 ways of adding CSS to HTML 
+ */
+// External CSS <head><link rel="stylesheet" href="mystyle.css" </head>
+// Internal CSS <style> body{ background-color: linen;} </style>
+// Inline CSS <h1 style="color:blue; text-align: center;">This is a heading </h1>
+
+/**
+ * 5. A,List the rules in the above code
+ *    B, List the style in the following code
+ *  A rule is the whole content inside the {}, one rule is one {}
+ * A list of style is the individual style with property and value example text-indent: 50px
+ */
+// p{ text-indent: 50px; text-transform:capitalize;}
+// h1{ text-shadow:2px 2px red;}
